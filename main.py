4e274e57446e49cbcd70193780cb5c3da5e6088c
LigianeBasques <ligianealzie25@gmail.com>
2021-11-13 20:30:12 -0300

    calculadora python

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # Arquivo principal
-
+print('***********Calculo da área das paredes de um cômodo************')
 
 largura = 5
 altura = 3
 profundidade = 2.9
+
+print(2 * (largura + profundidade) * altura)
