5a6915e061576bff756da57a5527dae85d0c2d02
LigianeBasques <ligianealzie25@gmail.com>
2021-11-14 13:29:31 -0300

    calculadora python

--- main.py
+++ main.py
@@ -5,11 +5,6 @@ largura = input('Qual a largura do cômodo? ')
 altura = 3
 profundidade = 2.9
 
-print(2 * (largura + profundidade) * altura)
-
-print('***********Calculo da área do teto************')
-print(largura * profundidade)
-
-print('***********inputs do usuário************')
+print(largura)
 
 
