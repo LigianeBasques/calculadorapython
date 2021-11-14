0c4d2ccae4ec0ca69084c3552beff9595294acfd
LigianeBasques <ligianealzie25@gmail.com>
2021-11-14 13:46:56 -0300

    calculadora python

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ largura: float = float(input('Qual a largura do cômodo? '))
 altura: float = float(input('Qual a profundidade do cômodo? '))
 profundidade: float = 2.9
 
-print((2*(largura + profundidade)*altura))
+print("A área das paredes é: ",(2*(largura + profundidade)*altura))
 
 
