8d2b4dce5bd9c2042dec0c6d1e9d9f308b9e9cff
LigianeBasques <ligianealzie25@gmail.com>
2021-11-15 11:08:40 -0300

    calculadora python

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 # Arquivo principal
 #Type Hint é um mecanismo do Python 3 através do qual podemos dar uma dica sobre o tipo de uma variável.
 print('***********Inputs do usuário************')
-
 from calculadora import Calculadora
+calc = Calculadora()
 
 largura: float = float(input('Qual a largura do cômodo? '))
 altura: float = float(input('Qual a profundidade do cômodo? '))
 profundidade: float = 2.9
+
 area_paredes:float = (2*(largura + profundidade)*altura)
 area_teto:float = largura * profundidade
 litragem:float = (area_paredes + area_teto) / 10
