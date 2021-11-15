2d210d7799327950bff5c40c10ad5e8d7ba708f9
LigianeBasques <ligianealzie25@gmail.com>
2021-11-15 11:10:18 -0300

    calculadora python

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ largura: float = float(input('Qual a largura do cômodo? '))
 altura: float = float(input('Qual a profundidade do cômodo? '))
 profundidade: float = 2.9
 
-area_paredes:float = (2*(largura + profundidade)*altura)
-area_teto:float = largura * profundidade
+calc.area_paredes:float = (2*(largura + profundidade)*altura)
+calc.area_teto:float = largura * profundidade
 litragem:float = (area_paredes + area_teto) / 10
 
-print("A área das paredes é: ",area_paredes)
-print("A área do teto é: ",area_teto)
+print("A área das paredes é: ",calc.area_paredes)
+print("A área do teto é: ",calc.area_teto)
 print("A litragem de tinta necessária é: ",area_teto)
 
 
