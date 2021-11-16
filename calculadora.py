f561ec25a515fe689f9dafe24c412de9a4982950
LigianeBasques <ligianealzie25@gmail.com>
2021-11-16 19:39:52 -0300

    calculadora python

--- calculadora.py
+++ calculadora.py
@@ -9,9 +9,13 @@ class Calculadora:
     def calcular_area_paredes(self,largura,profundidade,altura):
         self.area_paredes = 2 * (largura + profundidade) * altura
         return self.area_paredes
+
     def calcular_area_teto(self,largura,profundidade):
         self.area_teto = largura * profundidade
         return self.area_teto
+
+    def calcular_litragem_necessaria(self):
+
 
 
 
