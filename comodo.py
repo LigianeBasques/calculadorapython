31c9058a48215649c3b63703d88a6c8dfd234dea
LigianeBasques <ligianealzie25@gmail.com>
2021-11-20 17:13:57 -0300

    calculadora python

--- comodo.py
+++ comodo.py
@@ -1,4 +1,8 @@
 class Comodo:
+    largura: float
+    profundidade: float
+    altura: float
+
     def __init__(self, largura, profundidade):
         self.largura = float(largura)
         self.profundidade = float(profundidade)
